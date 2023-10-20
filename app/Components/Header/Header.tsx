35fb0f6c679b43d50f2a9edf1e4ed21776e2c1c1
Navilur Rahman <navilur.rahman@gmail.com>
2023-10-20 12:11:17 +0600

    header complete

---
+++ app/Components/Header/Header.tsx
@@ -0,0 +1,123 @@
+"use client";
+
+import Image from "next/image";
+import React from "react";
+
+import logo from "@/public/logo.png";
+import Button from "../Button/Button";
+import styled from "styled-components";
+
+import { FaRocket, FaWallet } from "react-icons/fa";
+
+function Header() {
+  return (
+    <HeaderStyled>
+      <nav>
+        <div className="logo">
+          <Image src={logo} alt="logo" width={36} />
+          <h2>Logo</h2>
+        </div>
+        <div className="input">
+          <input type="search" placeholder="Search" />
+        </div>
+        <ul className="nav_items">
+          <li>
+            <a href="">Home</a>
+          </li>
+          <li>
+            <a href="">Auctions</a>
+          </li>
+          <li>
+            <a href="">Marketplace</a>
+          </li>
+          <li>
+            <a href="">About</a>
+          </li>
+          <Button name="Connect Wallet" icon={<FaWallet />} />
+        </ul>
+      </nav>
+    </HeaderStyled>
+  );
+}
+
+const HeaderStyled = styled.header`
+  nav {
+    padding: 0 4rem;
+    min-height: 10vh;
+    border-bottom: 1px solid var(--color-border);
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    .logo {
+      display: flex;
+      align-items: center;
+      gap: 1rem;
+      cursor: pointer;
+    }
+    .input {
+      flex: 2;
+      display: flex;
+      justify-content: center;
+      input {
+        width: 55%;
+        padding: 0.6rem 0.8rem;
+        border-radius: 8px;
+        background-color: #161616;
+        border: 1px solid var(--color-border);
+        &::placeholder {
+          color: var(--color-border);
+          font-weight: 500;
+        }
+      }
+    }
+    .nav_items {
+      display: flex;
+      align-items: center;
+      gap: 2rem;
+      li {
+        transition: all 0.2s ease-in-out;
+
+        &:hover {
+          color: white;
+          transform: scale(1.1);
+        }
+      }
+    }
+  }
+  .header-content {
+    padding: 0 10rem 5rem 10rem;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    gap: 4rem;
+    min-height: calc(100vh - 10vh);
+
+    .text-content {
+      > h1 {
+        font-size: clamp(2rem, 5vw, 5rem);
+        color: #f2994a;
+        transition: all 0.01s linear;
+        padding-bottom: 1.5rem;
+      }
+
+      .buttons {
+        display: flex;
+        gap: 1rem;
+        margin-top: 2.5rem;
+      }
+    }
+
+    .image-content .image {
+      padding: 1rem;
+      border-radius: 8px;
+      background-color: var(--color-bg);
+      border: 1px solid var(--color-border);
+
+      img {
+        border-radius: 8px;
+      }
+    }
+  }
+`;
+
+export default Header;
